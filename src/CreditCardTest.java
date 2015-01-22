3c29d44f561093a613fe4ccd49985de0ce0f373a
kurosu49 <cam_05rules@hotmail.com>
2015-01-21 16:32:36 -0800

    Added updated CreditCardTest

--- src/CreditCardTest.java
+++ src/CreditCardTest.java
@@ -5,24 +5,25 @@ import org.junit.Test;
 
 
 public class CreditCardTest {
+	
+	private CreditCard card1;
+	private CreditCard card2;
 
 	@Before
 	public void setUp() throws Exception {
+		card1 = new CreditCard(new Person("Joe", "123", "A Town", "00000", "AH"));
+		card2 = new CreditCard(new Person("James", "456", "B Town", "00001", "BAH"), 3000);
 	}
 
 	@Test
-	public void testCreditCardPerson() {
-		fail("Not yet implemented");
+	public void testPayFalse() {
+		assertFalse(card2.pay(2900));
+	}
+	
+	@Test
+	public void testPayTrue() {
+		assertTrue(card1.pay(4000));
 	}
 
-	@Test
-	public void testCreditCardPersonDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPay() {
-		fail("Not yet implemented");
-	}
 
 }
